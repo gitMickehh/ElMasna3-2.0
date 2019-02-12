7535a469e80845ffe1790e367471fed2c4a514b7
Safwa <safwa.tawab@gmail.com>
2019-02-13 00:05:40 +0200

    Color Picker

---
+++ Assets/HSVPicker/Other/ColorPickerTester.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ColorPickerTester : MonoBehaviour 
+{
+
+    public new Renderer renderer;
+    public ColorPicker picker;
+
+    //public Color Color = Color.red;
+    public Color Color;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Color = renderer.material.color;
+        picker.onValueChanged.AddListener(color =>
+        {
+            renderer.material.color = color;
+            Color = color;
+        });
+
+		renderer.material.color = picker.CurrentColor;
+
+        picker.CurrentColor = Color;
+    }
+	
+}
---
+++ Assets/Scripts/UI/UICustomize.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICustomize : MonoBehaviour
+{
+    public GameObject colorPicker;
+
+    void Start()
+    {
+        colorPicker.SetActive(false);
+    }
+
+    public void OnCustomizeButtonClicked()
+    {
+        if (colorPicker.activeSelf)
+        {
+            colorPicker.SetActive(false);
+        }
+        else
+        {
+            colorPicker.SetActive(true);
+        }
+    }
+}
