689ef616ee69a351970951f8bdc1f0c8ec17361f
Nader <nader.ma@live.com>
2019-02-16 14:45:58 +0200

    Orientation Building script, Camera lerping to orientation and back

---
+++ Assets/Scripts/Camera/CameraLerping.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLerping : MonoBehaviour
+{
+    //lerping
+    private bool shouldLerp = false;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private float timeStartedLerping;
+    public float lerpTime = 1;
+
+    //quality of life
+    float lerpTimeTemp;
+
+    //old position
+    Vector3 originalPosition;
+    public Transform orientationPosition;
+    bool atOrientation;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+        lerpTimeTemp = lerpTime;
+
+        StartCoroutine(FindOrientationPosition());
+    }
+
+    IEnumerator FindOrientationPosition()
+    {
+        yield return new WaitForEndOfFrame();
+        orientationPosition = FindObjectOfType<OrientationBuilding>().RoomCamera;
+    }
+
+    public void SwitchView()
+    {
+        StopAllCoroutines();
+        shouldLerp = true;
+        timeStartedLerping = Time.time;
+        startPosition = transform.position;
+
+        if (atOrientation)
+        {
+            endPosition = originalPosition;
+            atOrientation = false;
+            lerpTimeTemp = lerpTime;
+            lerpTime /= 2;
+        }
+        else
+        {
+            endPosition = orientationPosition.position;
+            originalPosition.y = transform.position.y;
+            lerpTime = lerpTimeTemp;
+
+            atOrientation = true;
+        }
+
+        StartCoroutine(Lerping());
+    }
+
+    private IEnumerator Lerping()
+    {
+        while (shouldLerp)
+        {
+            transform.position = Lerp(startPosition, endPosition, timeStartedLerping, lerpTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    Vector3 Lerp(Vector3 start, Vector3 end, float timeStartedLerping, float lerpTime = 1)
+    {
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        var result = Vector3.Lerp(start, end, percentageComplete);
+
+        if (percentageComplete > 0.9f)
+            shouldLerp = false;
+
+        return result;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        if(orientationPosition != null)
+        {
+            Gizmos.DrawLine(transform.position, orientationPosition.position);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(orientationPosition.position, 0.5f);
+        }
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+    }
+}
---
+++ Assets/Scripts/Object/OrientationBuilding.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class OrientationPosition
+{
+    public Transform position;
+    public Worker worker;
+}
+
+public class OrientationBuilding : MonoBehaviour
+{
+    [Header("Camera")]
+    public Transform RoomCamera;
+
+    [Header("Worker Positions")]
+    public OrientationPosition[] WorkersPositions;
+
+}
