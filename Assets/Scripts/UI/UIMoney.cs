1daf8f350ffc74232895ed8eedff7bd19f089310
Safwa <safwa.tawab@gmail.com>
2019-02-22 13:56:03 +0200

    Currencies

---
+++ Assets/Scripts/UI/UIMoney.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMoney : MonoBehaviour
+{
+    public Currencies realMoney;
+    public Currencies happyMoney;
+
+    public Text realMoneyText;
+    public Text happyMoneyText;
+
+    public void OnRealMoneyChanged()
+    {
+        realMoneyText.text = realMoney.money.ToString();
+    }
+
+    public void OnHappyMoneyChanged()
+    {
+        happyMoneyText.text = happyMoney.money.ToString();
+    }
+}
