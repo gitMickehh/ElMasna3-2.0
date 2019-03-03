c4bda5601cda283bc29366d978dd879a68721d8b
Nader <nader.ma@live.com>
2019-03-03 13:35:01 +0200

    Character Profile added with animation but not functional yet

---
+++ Assets/Scripts/UI/UICharacterProfile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICharacterProfile : MonoBehaviour
+{
+    [Header("UI Objects")]
+    public Text CharacterName;
+    public Text CharacterLevel;
+    public Slider CharacterHappienss;
+    public Slider CharacterHealth;
+
+    [Header("Camera")]
+    public Camera UICamera;
+
+    //[Header("Worker")]
+    //[Attributes.GreyOut]
+    //public Worker WorkerRefernce;
+
+    public void FillInWorkerData(Worker w)
+    {
+        CharacterName.text = w.FullName;
+        CharacterLevel.text = w.level.ToString();
+        CharacterHappienss.value = (w.happyMeter/100.0f);
+
+
+    }
+}
