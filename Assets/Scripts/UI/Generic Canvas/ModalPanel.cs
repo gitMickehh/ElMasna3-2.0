4360ca54f6538433623ffca7476e6f57fedfa8cf
Safwa <safwa.tawab@gmail.com>
2019-03-06 02:09:02 +0200

    Generic Canvas

---
+++ Assets/Scripts/UI/Generic Canvas/ModalPanel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ModalPanel : MonoBehaviour
+{
+    public Text question;
+    public Image iconImage;
+    public Button yesButton;
+    public Button noButton;
+    public Button cancelButton;
+    public GameObject modalPanelObject;
+
+    private static ModalPanel modalPanel;
+
+    public static ModalPanel Instance()
+    {
+        if (!modalPanel)
+        {
+            modalPanel = FindObjectOfType(typeof(ModalPanel)) as ModalPanel;
+
+            if (!modalPanel)
+                Debug.LogError("There needs to be one active ModalPanel script on a GameObject in your scene. ");
+        }
+
+        return modalPanel;
+    }
+
+    public void Choice(string question, UnityAction yesEvent, UnityAction noEvent, UnityAction cancelEvent)
+    {
+        modalPanelObject.SetActive(true);
+
+        yesButton.onClick.RemoveAllListeners();
+        yesButton.onClick.AddListener(yesEvent);
+        yesButton.onClick.AddListener(ClosePanel);
+
+        noButton.onClick.RemoveAllListeners();
+        noButton.onClick.AddListener(noEvent);
+        noButton.onClick.AddListener(ClosePanel);
+
+        cancelButton.onClick.RemoveAllListeners();
+        cancelButton.onClick.AddListener(cancelEvent);
+        cancelButton.onClick.AddListener(ClosePanel);
+
+        this.question.text = question;
+
+        this.iconImage.gameObject.SetActive(false);
+        yesButton.gameObject.SetActive(true);
+        noButton.gameObject.SetActive(true);
+        cancelButton.gameObject.SetActive(true);
+
+    }
+
+    void ClosePanel()
+    {
+        modalPanelObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/UI/Generic Canvas/TestModalWindow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class TestModalWindow : MonoBehaviour
+{
+    private ModalPanel modalPanel;
+    private DisplayManager displayManager;
+
+    private UnityAction myYesAction;
+    private UnityAction myNoAction;
+    private UnityAction myCancelAction;
+
+    void Awake()
+    {
+        modalPanel = ModalPanel.Instance();
+        displayManager = DisplayManager.Instance();
+
+        myYesAction = new UnityAction(TestYesFunction);
+        myNoAction = new UnityAction(TestNoFunction);
+        myCancelAction = new UnityAction(TestCancelFunction);
+    }
+
+    public void TestYNC()
+    {
+        modalPanel.Choice("Would you like a poke in the eye? \nHow about with a sharp stick?", myYesAction, myNoAction, myCancelAction);
+    }
+    void TestYesFunction()
+    {
+        displayManager.DisplayMessage("Heck yeah! Yup!");
+    }
+
+    void TestNoFunction()
+    {
+        displayManager.DisplayMessage("No Way!");
+    }
+
+    void TestCancelFunction()
+    {
+        displayManager.DisplayMessage("I give up");
+    }
+}
