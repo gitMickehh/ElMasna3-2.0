8d23ba7cff8a8c8986130e75a41ce400dea51b02
Safwa <safwa.tawab@gmail.com>
2019-04-12 16:40:39 +0200

    Implementing functionalitiy of giveAbreak to worker and fixing animation bug

--- Assets/Scripts/UI/UICharacterProfile.cs
+++ Assets/Scripts/UI/UICharacterProfile.cs
@@ -41,6 +41,8 @@ public class UICharacterProfile : MonoBehaviour
     private ModalPanel modalPanel;
     private UnityAction GiveBreakAction;
 
+    public FloorList floorList;
+
     private void Start()
     {
         UICamera = Instantiate(CameraPrefab, new Vector3(), new Quaternion()).GetComponent<Camera>();
@@ -111,6 +113,29 @@ public class UICharacterProfile : MonoBehaviour
     {
         Debug.Log("Giving a break to " + SelectedWorkerRefernce.worker.FullName);
         //SelectedWorkerRefernce.worker.Break();
+
+        WayPoint floorWayPoint = floorList.GetFirstAvailableBreakSpace();
+
+        if (!floorWayPoint)
+        {
+            // modalPanel.
+            return;
+
+        }
+
+        Floor floor = floorList.GetFloorWithWayPoint(floorWayPoint);
+        int breakRoomIndex = floor.GetBreakRoomIndex(floorWayPoint);
+
+        SeekRoom seekRoom = SelectedWorkerRefernce.worker.GetComponent<SeekRoom>();
+        seekRoom.SwitchRoom(floorWayPoint, floor.WorkersHolder);
+
+        if (SelectedWorkerRefernce.worker.currentMachine != null)
+        {
+            SelectedWorkerRefernce.worker.currentMachine.CurrentWorker = null;
+            SelectedWorkerRefernce.worker.currentMachine = null;
+        }
+        SelectedWorkerRefernce.worker.SetWorking(false);
+        SelectedWorkerRefernce.worker.SetBreak(floor.breakRoom[breakRoomIndex].breakObject);
     }
 
 }
